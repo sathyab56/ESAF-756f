ee5afc2f4e4c65999c7660b345d5d7456a37be02
sathyab56 <sathyasubramaniam80@gmail.com>
2025-02-05 11:03:57 +0530

    lia

--- src/Components/Seal/Seal24/index.jsx
+++ src/Components/Seal/Seal24/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import "./index.css"; // Ensure you import the updated CSS file
+import "./styles.css";
 
 const StampDesign = () => {
   return (
-    <div className="seal24box">
-      <div className="seal24stamp">
-        <div className="seal24branch-name">
-          ESAF Small Finance Bank Ltd.
-          <p>Kuzhalmannam Branch</p>
+    <div className="container">
+      <div className="seal24box">
+        <div className="seal24stamp">
+          <div className="seal24branch-name">
+            ESAF Small Finance Bank Ltd.
+            <p>Kuzhalmannam Branch</p>
+          </div>
+          <div className="seal24pay">PAY</div>
+          <div className="seal24officer">Prescribed Officer</div>
         </div>
-        <div className="seal24pay">PAY</div>
-        <div className="seal24officer">Prescribed Officer</div>
       </div>
-      
     </div>
   );
 };
